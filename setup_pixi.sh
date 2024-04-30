a9f170a159179e76f1f6eb8fa7f4fb6b5edf93ac
Jonas Wuerzinger <jonas.wuerzinger@gmail.com>
2024-04-30 10:16:52 +0200

    adding pixi

---
+++ setup_pixi.sh
@@ -0,0 +1,14 @@
+# curl -fsSL https://pixi.sh/install.sh | bash
+# . ~/.bash_profile 
+# pixi init .
+time pixi add 'python=3.11'
+time pixi add gfortran
+time pixi add cmake
+
+# python packages on conda
+time pixi add numpy
+time pixi add pyyaml
+# time pixi add pyslha
+
+# start shell
+pixi shell
