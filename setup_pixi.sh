33ccc5ae21a8d4f8dfa9e1036ba3bb2b1a0c363e
Jonas Wuerzinger <jonas.wuerzinger@gmail.com>
2024-04-30 14:32:49 +0200

    adding make and cxx-compiler

--- setup_pixi.sh
+++ setup_pixi.sh
@@ -6,7 +6,10 @@ time pixi add gfortran
 time pixi add cc
 time pixi add gcc
 # time pixi add c++
+time pixi add cxx-compiler
 time pixi add cmake
+time pixi add make
+time pixi add which
 
 # python packages on conda
 time pixi add numpy
