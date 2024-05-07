9d654db84cd8d25774c88bb9223e1216f6d83f48
Jonas Wuerzinger <jonas.wuerzinger@gmail.com>
2024-05-07 14:00:37 +0200

    back to base version

--- source/micromegas_5.2.1/sources/maxGap/pathData.f
+++ source/micromegas_5.2.1/sources/maxGap/pathData.f
@@ -2,6 +2,6 @@
          BLOCK DATA 
          character*1000 PATH, FILENAME
          COMMON/maxGapPath/PATH,FILENAME
-         DATA PATH/'/afs/cern.ch/work/j/jwuerzin/pMSSMTaskforce/Run3ModelGen/source/micromegas_5.2.1/sources/maxGap'/
+         DATA PATH/'/home/pukhov/MICROMEGAS/2020/micromegas_5.2.1/sources/maxGap'/
          END
 
